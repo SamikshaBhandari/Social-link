9e6871305b953efe84923b413d0802407efcf6fd
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-30 08:18:56 +0545

    fix authicatation files

--- my-app/action/authAction.ts
+++ my-app/action/authAction.ts
@@ -1,15 +1,24 @@
 'use server'
-
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { createUserInDB, getUserByEmail } from '@/services/userServices';
 
-
-export async function loginAction(formData: FormData) {
+export async function loginAction(prevState: any, formData: FormData) {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+        return { success: false, message: "User not found" };
+    }
+
+    if (user.password !== password) {
+        return { success: false, message: "Invalid email or password" };
+    }
+
     const cookieStore = await cookies();
-    cookieStore.set('userId', '1', {
+    cookieStore.set('userId', String(user.id), {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24
@@ -18,16 +27,20 @@ export async function loginAction(formData: FormData) {
     redirect('/dashboard');
 }
 
-
-export async function registerAction(formData: FormData) {
-    const name = formData.get("name") as string;
+export async function registerAction(prevState: any, formData: FormData) {
+    const username = formData.get("username") as string;
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
+    try {
+        await createUserInDB({ username, email, password } as any);
+    } catch (error) {
+        console.error("Register Error:", error);
+        return { success: false, message: "Registration failed, try again!" };
+    }
 
     redirect('/login');
 }
-
 
 export async function logoutAction() {
     const cookieStore = await cookies();
